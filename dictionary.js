d06554a87ca61613b1d23151933206315bbf4f52
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-10-19 19:03:01 +0900

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -2,5 +2,16 @@ const dictionary = [
     { adyghe: 'псэ', english: 'water', transliteration: 'pse' },
     { adyghe: 'лъэпэ', english: 'fire', transliteration: 'lhepe' },
     { adyghe: 'шъу', english: 'tree', transliteration: 'sh’u' },
-    // 追加の単語をここに追加
+    { adyghe: 'щӏы', english: 'dog', transliteration: 'sh’ı' },
+    { adyghe: 'гуащэ', english: 'cat', transliteration: 'guashe' },
+    { adyghe: 'кӏалэ', english: 'horse', transliteration: 'k’alé' },
+    { adyghe: 'ху', english: 'cow', transliteration: 'khu' },
+    { adyghe: 'бзылъ', english: 'wolf', transliteration: 'bzylh' },
+    { adyghe: 'хъуакӏу', english: 'fox', transliteration: 'kh’wak’u' },
+    
+    { adyghe: 'гъэщхьэ', english: 'bee', transliteration: 'gheshhe' },
+    { adyghe: 'чӏэлэ', english: 'ant', transliteration: 'ch’ele' },
+    { adyghe: 'шъуней', english: 'butterfly', transliteration: 'sh’uney' },
+    { adyghe: 'кӏуакӏу', english: 'spider', transliteration: 'k’uak’u' },
+    { adyghe: 'пӏы', english: 'fly', transliteration: 'p’y' }
 ];
