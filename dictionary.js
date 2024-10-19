c752023b7eb8f01b715307951a7222b038d44486
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-10-19 18:53:55 +0900

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,6 @@
+const dictionary = [
+    { adyghe: 'псэ', english: 'water', transliteration: 'pse' },
+    { adyghe: 'лъэпэ', english: 'fire', transliteration: 'lhepe' },
+    { adyghe: 'шъу', english: 'tree', transliteration: 'sh’u' },
+    // 追加の単語をここに追加
+];
